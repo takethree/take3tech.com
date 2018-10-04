32b17c37538f6915c0b051393a98df9136c82d06
Brad Butner <bbutner@take3tech.com>
2018-10-04 14:14:20 -0600

    Added base site code

---
+++ js/contact_demoRequest.js
@@ -0,0 +1,71 @@
+/*
+  Jquery Validation using jqBootstrapValidation
+   example is taken from jqBootstrapValidation docs 
+  */
+$(function() {
+
+ $("input,select,textarea").jqBootstrapValidation(
+    {
+     preventSubmit: true,
+     submitError: function($form, event, errors) {
+      // something to have when submit produces an error ?
+      // Not decided if I need it yet
+     },
+     submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+       // get values from FORM
+       var name = $("input#name2").val();  
+       var email = $("input#email2").val();
+       var tel = $("input#tel2").val();
+       var subject = $("select#subject").val();
+       var firstName = name; // For Success/Failure Message
+           // Check for white space in name for Success/Fail message
+       if (firstName.indexOf(' ') >= 0) {
+     firstName = name.split(' ').slice(0, -1).join(' ');
+         }        
+   $.ajax({
+              url: "./PHP/contact_demoRequest.php",
+              type: "POST",
+              data: {name: name, email: email, subject: subject, tel: tel},
+              cache: false,
+              success: function() {  
+              // Success message
+                $('#success2').html("<div class='alert alert-success'>");
+                $('#success2 > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                .append( "</button>");
+                $('#success2 > .alert-success')
+                .append("<strong>Your message has been sent. Thanks!</strong>");
+                $('#success2 > .alert-success')
+                .append('</div>');
+                
+      //clear all fields
+      $('#demoForm').trigger("reset");
+        },
+     error: function() {    
+    // Fail message
+     $('#success2').html("<div class='alert alert-danger'>");
+              $('#success2 > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+               .append( "</button>");
+              $('#success2 > .alert-danger').append("<strong>Sorry "+firstName+" it seems that our mail server is not responding...</strong> Could you please email us directly to <a href='mailto:support@take3tech.com?Subject=Please Contact "+firstName+" - Take 3 Support'>support@take3tech.com</a>? Sorry for the inconvenience!");
+          $('#success2 > .alert-danger').append('</div>');
+    //clear all fields
+    $('#demoForm').trigger("reset");
+      },
+           })
+         },
+         filter: function() {
+                   return $(this).is(":visible");
+         },
+       });
+
+      $("a[data-toggle=\"tab\"]").click(function(e) {
+                    e.preventDefault();
+                    $(this).tab("show");
+        });
+  });
+ 
+
+/*When clicking on Full hide fail/success boxes */ 
+$('#name2').focus(function() {
+     $('#success2').html('');
+  });
---
+++ js/take3_main.js
@@ -0,0 +1,42 @@
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// Auto Format Phone Number
+jQuery(function($) {
+   $("#tel, #tel2").mask("?(999) 999-9999", {
+    placeholder: " "
+  });
+});
+// Banner Ad Top
+// $('#topNavBar').affix({
+//     offset: {
+//         top: $('.bannerAd').height()
+//     }   
+// });
+
+// jQuery for page scrolling feature - jQueryEasing
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+    $('.navbar-toggle:visible').click();
+  }
+});
